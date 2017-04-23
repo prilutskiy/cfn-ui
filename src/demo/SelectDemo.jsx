804a2af6adf182db6968b86f6555bd3712ed7550
Max Prilutskiy <maks.prilutskiy@gmail.com>
2017-04-23 16:11:40 +0300

    Select stub

--- src/demo/SelectDemo.jsx
+++ src/demo/SelectDemo.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Select from './../components/Select';
+import Button from './../components/Button';
+import Box from './../components/Box';
 
 class SelectDemo extends Component {
   constructor(props) {
@@ -17,55 +19,25 @@ class SelectDemo extends Component {
   render() {
     return (
       <div>
-        <div>
-          <Select default value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select default icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select default multi value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select default multi icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-        </div>
-        <div>
-          <Select disabled primary value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select disabled primary icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select disabled primary multi value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select disabled primary multi icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-        </div>
-        <div>
-          <Select primary value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select primary icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select primary multi value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select primary multi icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-        </div>
-        <div>
-          <Select success value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select success icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select success multi value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select success multi icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-        </div>
-        <div>
-          <Select info value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select info icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select info multi value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select info multi icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-        </div>
-        <div>
-          <Select warning value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select warning icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select warning multi value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select warning multi icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-        </div>
-        <div>
-          <Select danger value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select danger icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select danger multi value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select danger multi icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-        </div>
-        <div>
-          <Select xs primary value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select sm primary icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select md primary multi value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select lg primary multi icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-          <Select xl primary multi icon="building" value={this.state.values} onChange={(o) => this.handlSelectChange(o)} options={this.options} />
-        </div>
+        <Box>
+          <Box.Header>Selects</Box.Header>
+          <Box.Content>
+            <div>
+              <Select placeholder="Username" type="text" icon="user" />
+              <Button primary>Action</Button>
+              <Button primary clean>Reset</Button>
+            </div>
+            <div>
+              <Select placeholder="Password" type="password" icon="key" />
+              <Button primary>Action</Button>
+              <Button primary clean>Reset</Button>
+            </div>
+            <div>
+              <Select success placeholder="Text here" />
+              <Select danger highlighted placeholder="Password" type="password" icon="key" />
+            </div>
+          </Box.Content>
+        </Box>
       </div>
     );
   }
