52db231f95e184c3ff7f3671b9844035cf04d25e
Max Prilutskiy <maks.prilutskiy@gmail.com>
2017-04-24 17:21:02 +0300

    Overlay for sidemenu

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -19,22 +19,26 @@ class SideMenu extends Component {
 
   render() {
     return (
-      <div className={this.getClassSet()}>
-        <div className="sidemenu-header">
-          <div className="sidemenu-brand">
-            <img className="sidemenu-brand-logo" src="/bg.png" alt="" />
-            <h4 className="sidemenu-brand-title">
-              Brand
-            </h4>
-          </div>
-          <div className="sidemenu-dismiss" onClick={this.props.onClose}>
-            <i className="fa fa-times" />
-          </div>
+      <div>
+        <div className={'overlay overlay-page overlay-default ' + (this.props.show ? 'overlay-opened' : '')} onClick={this.props.onClose}>
         </div>
-        <div className="sidemenu-list">
-          <div className="sidemenu-list-item">First Link</div>
-          <div className="sidemenu-list-item">Second Link</div>
-          <div className="sidemenu-list-item">Third Link</div>
+        <div className={this.getClassSet()}>
+          <div className="sidemenu-header">
+            <div className="sidemenu-brand">
+              <img className="sidemenu-brand-logo" src="/bg.png" alt="" />
+              <h4 className="sidemenu-brand-title">
+                Brand
+            </h4>
+            </div>
+            <div className="sidemenu-dismiss" onClick={this.props.onClose}>
+              <i className="fa fa-times" />
+            </div>
+          </div>
+          <div className="sidemenu-list">
+            <div className="sidemenu-list-item">First Link</div>
+            <div className="sidemenu-list-item">Second Link</div>
+            <div className="sidemenu-list-item">Third Link</div>
+          </div>
         </div>
       </div>
     );
